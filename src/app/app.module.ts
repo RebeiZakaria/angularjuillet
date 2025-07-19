c4fdc588a7b60ee51e21ee12936b4ac3abf743ab
Rebei Zakaria <zikourebei1003@gmail.com>
2025-07-19 17:39:00 +0100

    seance2 sans netlify

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderfooterComponent } from './headerfooter/headerfooter.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeaderfooterComponent,
+    HomeComponent,
+    UsersComponent,
+    ProductsComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent {
+title:string ="welcome to users page";
+imageUrl ='assets/img/bank-image.png';
+property:boolean=false;
+name= "zikoo";
+isVisible =false;
+items:string[]=["item1","item2","item3"];
+buttonClass:string='color'
+togglecolor() {
+  this.buttonClass= this.buttonClass==='color'?'couleur':'color';
+  }
+onClick() {
+  alert('button clicked')
+  }
+color:string[]=['purple','blue','red','green','orange','pink']
+currentColor: string= this.color[0];
+colorIndex: number=0;
+changeColor() {
+  this.colorIndex=(this.colorIndex+1)% this.color.length;
+  this.currentColor=this.color[this.colorIndex]
+  }
+}
